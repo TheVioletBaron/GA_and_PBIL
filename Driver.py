b5a88c588878426fb87ffa97241518ca320ebc45
violetbaron <cedmonds2018@gmail.com>
2021-03-06 15:53:34 -0500

    Updated plan

--- Driver.py
+++ Driver.py
@@ -1,1 +1,5 @@
-TODO: write driver code
+"""
+Driver contains a main method which creates a GA object and a PBIL object.
+Those then run their respective programs and output their results, times, etc.
+Driver then formats/prints/saves those outputs as needed.
+"""
