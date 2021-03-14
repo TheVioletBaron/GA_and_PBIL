49cb6d9de50d1f73a44c217c5e5166e6a88018b9
Jasper <jgordon3@bowdoin.edu>
2021-03-14 14:09:39 -0400

    Just starting some GA work

--- GA.py
+++ GA.py
@@ -1,3 +1,30 @@
-'''
-TODO: Write GA
-'''
+#Aly and Jasper
+import random
+
+
+solution_count = 10
+varible_count = 5
+
+
+
+
+count = 0
+solution_list = []
+
+for i in range(1, solution_count):
+    individual = ""
+    for bit in range (1, varible_count):
+        new_bit = str(random.randrange(0, 2))
+        individual = individual + new_bit
+    solution_list.append(individual)
+
+
+print (solution_list)
+
+
+
+
+
+
+
+
