9ff7d9f5751b0a3b8bff76f289c21cd1bb061c04
Alexandra Hummel <aly.hummel@gmail.com>
2021-03-22 14:25:52 -0400

    Individual
    Adding Individual class

--- file_reading.py
+++ file_reading.py
@@ -23,65 +23,62 @@ clause_num = int(first_line[3])
 #adding in the GA random solution generation code so we can do everything together
 
 solution_count = 5
-#varible_count = var_num
 varible_count = var_num
 
 count = 0
 solution_list = []
 
 for i in range(1, solution_count):
-    individual = ""
+    bitString = ""
     for bit in range (0, varible_count):
         new_bit = str(random.randrange(0, 2))
-        individual = individual + new_bit
-    solution_list.append(individual)
+        bitString = bitString + new_bit
+    solution_list.append(Individual(-1, bitString))
 print("solution list:")
 print(solution_list)
 
 #Method to compare a clause of literals with an indivudal solution bit string
 def check_score(solution, clause):
-    print(clause)
+    #print(clause)
     try:
         for literal in clause:
-        # if (literal == "p"):
-        #     print("GOTCHA")
-        #     print(clause)
             positive_literal = int(literal) > 0 # check if literal j is positive or negative, maybe later jsut check if first char is - instead of casting
             good_value = "0"
             if (positive_literal):
                 good_value = "1"
-            if (solution[abs(int(literal)) - 1] != good_value):
+            if (solution.bitString[abs(int(literal)) - 1] != good_value):
                 return False
     except:
         print("oh no")
         print(clause)
         print(solution)
-        print("length" + str(len(solution)))
+        print("length" + str(len(solution.bitString)))
         print("")
 
     return True
 
 # end random solution generator
-fitness = [0] * solution_count
-# for line in lines:
-# print("lines:")
 for p in range(1, clause_num):
     line = lines[p]
     # print(line)
     literals_list = line.split() #  list of litersls
     for  i in range (0, len(solution_list)): # iterate through each individual i
         if (check_score(solution_list[i], literals_list)):
-                fitness[i] += 1
+                #fitness[i] += 1
+                solution_list[i].fitness += 1
  
 # test_solution = "011"
 # tett_clause = ["1", "2", "3"]
 # print(check_score(test_solution, tett_clause))
 
 print("individuals:")
-print(solution_list)
+for individual in solution_list:
+    print(individual.bitString)
+    print(individual.fitness)
 
-print("\nfitness:")
-print(fitness)
+#print("\nfitness:")
+#print(fitness)
+
 
     
 
