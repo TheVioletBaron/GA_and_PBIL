ac19ecabcee9bc188ab0c0586ec303511f76423d
Jasper <jgordon3@bowdoin.edu>
2021-03-22 16:54:48 -0400

    GAAAAA

--- GA.py
+++ GA.py
@@ -1,27 +1,31 @@
 #Aly and Jasper
 import random
+from Individual import Individual
+from math import *
+
+# solution_count = 10
+# varible_count = 5
+
+# count = 0
+# solution_list = []
+
+# for i in range(1, solution_count):
+#     individual = ""
+#     for bit in range (1, varible_count):
+#         new_bit = str(random.randrange(0, 2))
+#         individual = individual + new_bit
+#     solution_list.append(individual)
 
 
-solution_count = 10
-varible_count = 5
-
-count = 0
-solution_list = []
-
-for i in range(1, solution_count):
-    individual = ""
-    for bit in range (1, varible_count):
-        new_bit = str(random.randrange(0, 2))
-        individual = individual + new_bit
-    solution_list.append(individual)
-
-
-print (solution_list)
-
-
+# print (solution_list)
 
 class GA(object):
     
+    var_num = 0
+    clause_num = 0
+    solution_list = []
+    total_probability = 0
+
     def __init__(self, file, popSize, select, cross_method, cross_prob, mut_prob, generations):
         self.file = file
         self.popSize = popSize
