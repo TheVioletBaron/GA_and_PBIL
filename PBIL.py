e7aae71f2ff5cbc875be7164a0e5ab308add6b5c
Casey <cedmonds2018@gmail.com>
2021-03-23 17:56:11 -0400

    Fixed bugs in sample generation, eval

--- PBIL.py
+++ PBIL.py
@@ -24,6 +24,34 @@ Population Based Incremental Learning
 
 class Pbil(object):
     
+
+    def evaluate_fitness(self, individual):
+        cnf = open(self.file)
+        fitness = 0
+        lines = cnf.readlines()
+        start_index = 0
+        for line in lines:
+                has = re.match("(-?[0-9]+| )+", line)
+                if (has):
+                        break
+                start_index = start_index + 1
+        lines = lines[start_index:]
+        for line in lines:
+                line = line.strip()
+                clause = line.split()
+                index = 0
+                while(clause[index] != "0"):
+                        #While loop takes the variables in each clause, finds the corresponding variable in the individual, and
+                        #compares the two to update the fitness accordingly
+                        variable = individual[abs(int(clause[index]))-1] #Take absolute value of to find index of variable in individual
+                        if(variable == 1 and int(clause[index])>0):
+                                fitness += 1
+                        elif(variable == 0 and int(clause[index])<0):
+                                fitness += 1
+                        index += 1
+        return fitness
+
+
     def __init__(self, file, popSize, alphaBest, alphaWorst, mProb, mShift, iters):
         """
         """
@@ -39,16 +67,15 @@ class Pbil(object):
         lines = cnf.readlines()
         length = 0
         for line in lines:
-                has = re.match("(-?[0-9]+| )+", line)
+                has = re.search("[0-9]+", line)
                 if (has):
-                       length = len(has.group().split())
+                       length = int(has.group().split()[0])
                        break
         pv = [0.5] * length
-
+        print(length)
         samples = []
         while self.iters:
                 while len(samples) < self.popSize:
-                        print("f")
                         sample = []
                         for i in pv:
                                 if (random.random() > i):
@@ -56,38 +83,22 @@ class Pbil(object):
                                 else:
                                         sample.append(1)
                         samples.append(sample)
-                self.iters = self.iters - 1
-
-	#store Probability Vector as probVec List
+                best = 0
+                worst = length
+                for sample in samples:
+                        s_fit = self.evaluate_fitness(sample)
+                        if s_fit > best:
+                                best = s_fit
+                        if s_fit < worst:
+                                worst = s_fit
+                print(best)
 	#for sample in samples
 	#bestvec
 	#worstvec
 	#update toward
 	#update away
 	#mutate
+                self.iters = self.iters - 1
 
 
 
-
-        
-    def evaluate_fitness(individual):
-        cnf = open(self.file)
-        fitness = 0
-        Lines = f.readlines()
-        
-        # TODO: Add loop to iterate through lines until the "p cnf \d \d" line is found
-        
-        for line in Lines:
-            line = line.strip()
-            clause = line.split()
-            index = 0
-            while(clause[index] != "0"):
-               #While loop takes the variables in each clause, finds the corresponding variable in the individual, and
-               #compares the two to update the fitness accordingly
-                variable = individual[abs(int(clause[index]))-1] #Take absolute value of to find index of variable in individual
-                if(variable == 1 and int(clause[index])>0):
-                    fitness += 1
-                elif(variable == 0 and int(clause[index])<0):
-                    fitness += 1
-                index += 1
-        return fitness
