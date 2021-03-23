bb556ab7084d2fbe9eb950f41f05f5e060a61108
Jasper <jgordon3@bowdoin.edu>
2021-03-23 10:45:21 -0400

    Adding Aly's work

---
+++ Individual.py
@@ -0,0 +1,11 @@
+class Individual:
+    fitness = 0
+    bitString = ""
+    probability = 0
+
+    def __init__(self, fitness, bitString):
+        self.fitness = fitness
+        self.bitString = bitString
+
+    def setProb(self, newProbability):
+        this.probability = newProbability
