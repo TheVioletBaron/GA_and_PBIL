be5adad3680ff183dea379d7d5e2d23ddf846b06
batal <batal@139.140.203.135>
2021-03-28 05:24:57 -0400

    Fixed Fitness Eval.  PV is still being updated too much per iteration

--- PBIL.py
+++ PBIL.py
@@ -25,29 +25,30 @@ Population Based Incremental Learning
 
 class Pbil(object):
     
-
-    def evaluate_fitness(self, individual):
+        #Fixed, previous version was returning low fitness
+    def evaluate_fitness(self, individual): 
         cnf = open(self.file)
         fitness = 0
         lines = cnf.readlines()
         start_index = 0
         for line in lines:
                 has = re.match("(-?[0-9]+| )+", line)
-                if (has):
+                if (has): 
                         break
                 start_index = start_index + 1
         lines = lines[start_index:]
-        for line in lines:
-                clause = line.strip().split()
+        for line in lines: 
+                clause = line.strip().split() 
                 passed = True
-                for cv in clause:
-                        sv = individual[abs(int(cv)) - 1]
-                        if not ((sv == 1) and (int(cv) > 0)) or ((sv == 0) and (int(cv) < 0)):
-                                if cv != '0':
-                                        passed = False
+                for cv in clause[:-1]: #changed so Ignores 0
+                        sv = individual[abs(int(cv)) - 1] 
+                        if sv == 0 and int(cv) > 0: 
+                                passed = False
+                        if sv == 1 and int(cv) < 0:
+                                passed = False
                 if passed:
                         fitness = fitness + 1
-        return fitness
+        return fitness #Fitness is always returned correctly when using TestProb.cnf
 
 
     def __init__(self, file, popSize, alphaBest, alphaWorst, mProb, mShift, iters):
@@ -82,19 +83,25 @@ class Pbil(object):
         worst = []
         max_iters = self.iters
         while self.iters:
-                while len(samples) < self.popSize:
+                while len(samples) < self.popSize: #building correct number of samples
+                        
                         sample = []
                         for i in pv:
-                                if (random.random() < i):
+                                if (random.random() > i): #Changed: to > because probability in pv is for bit being 1? 
                                         sample.append(0)
                                 else:
                                         sample.append(1)
+                                
                         samples.append(sample)
+                        
                 best_fit = 0
                 worst_fit = max_fit + 1
                 best = []
                 worst = []
                 for sample in samples:
+                        #Edited to test evalFitness()
+                        #sample = [1, 1, 0, 0, 0, 1, 0, 1, 0] #testing with TestProb.cnf should have fitness of 1 but gets 0
+
                         s_fit = self.evaluate_fitness(sample)
                         if s_fit > best_fit:
                                 best_fit = s_fit
@@ -102,11 +109,13 @@ class Pbil(object):
                         if s_fit < worst_fit:
                                 worst_fit = s_fit
                                 worst = sample
+                        #print(best_fit)
                 for i in range(length):
-                        pv[i] = pv[i]*(1 - self.alphaBest) + best[i] * self.alphaBest
+                        pv[i] = pv[i]*(1 - self.alphaBest) + best[i] * self.alphaBest #pv appears to be being updated too 
                 for i in range(length):
                         if best[i] != worst[i]:
                                 pv[i] = pv[i]*(1 - self.alphaWorst) - worst[i] * self.alphaWorst
+                #print(pv)
                 i = 0
                 while(i < len(pv)): #Mutation
                         if(random.random() < self.mProb):
@@ -120,8 +129,12 @@ class Pbil(object):
                 if (self.evaluate_fitness(best) == max_fit):
                         break
 
+
+        
         self.final_iter = max_iters - self.iters
         self.final_count = self.evaluate_fitness(best)
         self.best = best
         self.max_fit = max_fit
         self.length = length
+
+
